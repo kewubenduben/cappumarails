68276f83bf68538c86c3abd23e2c26118a5e029a
Ker Ruben Ramos <xdiscent@gmail.com>
2016-05-30 03:34:07 +0800

    init 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,34 @@ set :deploy_to, '/home/vagrant/cappumarails'
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.3.1'
 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
 namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+  
   before :migrate, 'db:create'
 
   after :restart, :clear_cache do
@@ -49,5 +76,7 @@ namespace :deploy do
       # end
     end
   end
-
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
 end
