0a0143373b0b3236179c657fde9a85ef5e741e25
Ker Ruben Ramos <xdiscent@gmail.com>
2016-05-30 21:11:10 +0800

    init 8

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'cappumarails'
-set :repo_url, 'git@github.com:kewubenduben/cappumarails.git'
+set :repo_url, 'https://github.com/kewubenduben/cappumarails.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
